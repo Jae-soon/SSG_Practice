2b793e266fa611172f3197ca0a6746be14afbd65
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-15 13:15:25 +0900

    exit

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,7 +1,25 @@
 package com.ll.exam;
 
+import java.util.Scanner;
+
 public class App {
+    String cmd;
+    Scanner sc;
+
+    App() {
+        sc = new Scanner(System.in);
+    }
+
     public void run() {
         System.out.println("== 명언 SSG ==");
+
+        System.out.print("cmd) ");
+        cmd = sc.nextLine().trim();
+
+        outer:
+        switch(cmd) {
+            case "exit":
+              break outer;
+        }
     }
 }
