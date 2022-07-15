69c645a44e540cab9affe525aaca4decbd08a632
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-15 13:33:16 +0900

    write

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,25 +1,49 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     String cmd;
     Scanner sc;
+    List<WiseSaying> wiseSaying;
+    int lastIndex;
 
     App() {
         sc = new Scanner(System.in);
+        wiseSaying = new ArrayList<>();
+        lastIndex = 0;
     }
 
     public void run() {
         System.out.println("== 명언 SSG ==");
 
-        System.out.print("cmd) ");
-        cmd = sc.nextLine().trim();
-
         outer:
-        switch(cmd) {
-            case "exit":
-              break outer;
+        while(true) {
+            System.out.print("cmd) ");
+            cmd = sc.nextLine().trim();
+
+            switch(cmd) {
+                case "exit":
+                    break outer;
+                case "write":
+                    write();
+                    break;
+            }
         }
+    }
+
+    private void write() {
+        int id = ++lastIndex;
+        System.out.print("wiseSaying : ");
+        String content = sc.nextLine().trim();
+        System.out.print("author : ");
+        String author = sc.nextLine().trim();
+
+        WiseSaying newWiseSaying = new WiseSaying(id, content, author);
+        wiseSaying.add(newWiseSaying);
+
+        System.out.printf("Add Your WiseSaying(ID = %d)\n", newWiseSaying.id);
     }
 }
