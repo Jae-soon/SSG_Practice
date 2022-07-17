7d0d51b048edbd0070184bd6ad59920f91421170
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-07-17 19:14:12 +0900

    write and read file

--- src/main/java/com/ll/exam/WiseSaying.java
+++ src/main/java/com/ll/exam/WiseSaying.java
@@ -11,5 +11,27 @@ public class WiseSaying {
         this.author = author;
     }
 
+    @Override
+    public String toString() {
+        return "WiseSaying{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public String toJson() {
+        return """
+                {
+                    "id": %d,
+                    "content": "%s",
+                    "author": "%s"
+                }
+                """
+                .stripIndent()
+                .formatted(id, content, author)
+                .trim();
+
+    }
 
 }
